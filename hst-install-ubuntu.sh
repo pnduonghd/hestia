7500fb5f1a2d948e58b83fd93ee073a433d4c942
git <git@git-scm.com>
2023-08-15 13:00:50 +0700

    Update

--- hst-install-ubuntu.sh
+++ hst-install-ubuntu.sh
@@ -33,7 +33,7 @@ VERBOSE='no'
 # Define software versions
 HESTIA_INSTALL_VER='1.8.5'
 # Dependencies
-multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2")
+multiphp_v=("5.6" "7.3" "8.2")
 fpm_v="8.2"
 mariadb_v="10.11"
 
